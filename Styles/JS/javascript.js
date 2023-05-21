d3d601e01776630c29557cbdcc538a09dff8d4a4
Kambale Kibeho <dessaikibeho@gmail.com>
2023-05-21 19:00:58 +0300

    vis

--- Styles/JS/javascript.js
+++ Styles/JS/javascript.js
@@ -2,3 +2,8 @@ window.addEventListener('scroll',function(){
         var header=document.querySelector('header');
         header.classList.toggle("sticky",window.scrollY>10);
     } )
+    $(".option").click(function(){
+        $(".option").removeClass("active");
+        $(this).addClass("active");
+        
+     });
